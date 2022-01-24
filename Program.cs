56d1c9d2f74e6af0b0ff077cfebd3f2ce2bfaadc
Dragana Nikic <dragananikic@usf.edu>
2022-01-23 20:27:45 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Programer: Dragana Nikic
+// Project: Tech Assignment #2 - Basic Console App
+// Due Date: 01/28/2022
+// Description: Individual Tech #2 Assignment 
+
+using System;
+
+namespace TechAssignment2_BasicConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ISM4300 is fun! Work hard and you will do well! Submitted by Dragana Nikic");
+        }
+    }
+}
